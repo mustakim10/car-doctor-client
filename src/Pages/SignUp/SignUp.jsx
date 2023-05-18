d0fa2f1cf8c55c4ffb17ada3cc28ba3c12d78b97
Mustakim <sycospike1@gmail.com>
2023-05-18 17:03:24 +0600

    added auth provideer

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,11 +1,12 @@
-import { Link } from 'react-router-dom';
-import img from '../../assets/images/login/login.svg'
 import { useContext } from 'react';
+import img from '../../assets/images/login/login.svg'
+import { Link } from 'react-router-dom';
 import { AuthContext } from '../../providers/AuthProvider';
 
-const Login = () => {
-    const {logIn}= useContext(AuthContext);
-    const handleLogin = event => {
+const SignUp = () => {
+
+    const {createUser} = useContext(AuthContext);
+    const handleSignUp = event => {
         event.preventDefault();
         const form = event.target ;
         const name = form.name.value;
@@ -13,12 +14,12 @@ const Login = () => {
         const password = form.password.value;
         console.log(name,email,password);
 
-        logIn(email,password)
+        createUser(email,password)
         .then(result => {
             const user = result.user;
-            console.log(user);
+            console.log(user)
         })
-        .catch(error => console.log(error));
+        .catch(error => console.log(error))
     }
     return (
         <div className="hero min-h-screen bg-base-200">
@@ -29,8 +30,14 @@ const Login = () => {
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <div className="card-body">
-                    <h1 className="text-3xl text-center font-bold">Login </h1>
-                        <form onSubmit={handleLogin}>
+                    <h1 className="text-3xl text-center font-bold">Sign Up </h1>
+                        <form onSubmit={handleSignUp}>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" name='name' placeholder="name" className="input input-bordered" />
+                        </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
@@ -39,7 +46,7 @@ const Login = () => {
                         </div>
                         <div className="form-control">
                             <label className="label">
-                                <span className="label-text">Password</span>
+                                <span className="label-text">Confirm Password</span>
                             </label>
                             <input type="text" name='password' placeholder="password" className="input input-bordered" />
                             <label className="label">
@@ -48,10 +55,10 @@ const Login = () => {
                         </div>
                         <div className="form-control mt-6">
                            
-                            <input className='btn  btn-primary' type="submit" value="Login" />
+                            <input className='btn  btn-primary' type="submit" value="Sign Up" />
                         </div>
                         </form>
-                        <p className='text-center my-5'>New to Cars Doctor <Link className='text-orange-500 font-bold' to="/signup">Sign Up</Link> </p>
+                        <p className='text-center my-5'>Already have an account <Link className='text-orange-500 font-bold' to="/login">Login</Link> </p>
                     </div>
                 </div>
             </div>
@@ -59,4 +66,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default SignUp;
--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,14 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import router from './routes/Routes.jsx';
+import AuthProvider from './providers/AuthProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <div className='max-w-7xl mx-auto'>
     <React.StrictMode>
-      <RouterProvider router={router} />
+     <AuthProvider>
+     <RouterProvider router={router} />
+     </AuthProvider>
     </React.StrictMode>,
   </div>
 )
